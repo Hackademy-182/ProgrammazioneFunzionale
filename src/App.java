e88a22255f1d77126c569b977cc9a74bd76d1884
Tommaso Mancuso <tjmancuso@virgilio.it>
2026-04-15 16:43:32 +0200

    corretto esercizio mele

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 
 public class App {
@@ -14,16 +16,41 @@ public class App {
             new Apple (Color.green, 147)
         );
 
-        List<Apple> redApples = apples.stream().filter(a -> a.getColor().equals(Color.red)).toList();
-        List<Apple> greenApples = apples.stream().filter(a -> a.getColor().equals(Color.green)).toList();
-        List<Apple> lightApples = apples.stream().filter(a -> a.getWeight() < 150).toList();
-        List<Apple> heavyApples = apples.stream().filter(a -> a.getWeight() >= 150).toList();
+        // List<Apple> redApples = apples.stream().filter(a -> a.getColor().equals(Color.red)).toList();
+        // List<Apple> greenApples = apples.stream().filter(a -> a.getColor().equals(Color.green)).toList();
+        // List<Apple> lightApples = apples.stream().filter(a -> a.getWeight() < 150).toList();
+        // List<Apple> heavyApples = apples.stream().filter(a -> a.getWeight() >= 150).toList();
 
-        System.out.println("Le mele rosse sono: " + redApples.size());
-        System.out.println("Le mele verdi sono: " + greenApples.size());
-        System.out.println("Le mele leggere sono: " + lightApples.size());
-        System.out.println("Le mele pesanti sono: " + heavyApples.size());
+        
 
+        // System.out.println("Le mele rosse sono: " + redApples.size());
+        // System.out.println("Le mele verdi sono: " + greenApples.size());
+        // System.out.println("Le mele leggere sono: " + lightApples.size());
+        // System.out.println("Le mele pesanti sono: " + heavyApples.size());
+
+        
+        List<Apple> redApples = filterApples(apples, (Apple a) -> a.getColor().equals(Color.red));
+        List<Apple> greenApples = filterApples(apples, (Apple a) -> a.getColor().equals(Color.green));
+        List<Apple> lightApples = filterApples(apples, (Apple a) -> a.getWeight() < 150);
+        List<Apple> heavyApples = filterApples(apples, (Apple a) -> a.getWeight() >= 150);
+
+        System.out.println("Rosse: " + redApples.size());
+        System.out.println("Verdi: " + greenApples.size());
+        System.out.println("Leggere: " + lightApples.size());
+        System.out.println("Pesanti: " + heavyApples.size());
+        
     }
+
+    public static List<Apple> filterApples (List<Apple> apples, Predicate<Apple> predicate){
+        List<Apple> result = new ArrayList<>();
+        for (Apple apple : apples) {
+            if (predicate.test(apple)) {
+                result.add(apple);
+            }
+        }
+
+        return result;
+    }
+
 
 }
